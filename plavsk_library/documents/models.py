80f3c0fa8bfd49173308ee81890efe048e7aaafb
Kapusta-fairy <dknenorm63@gmail.com>
2022-09-20 11:08:56 +0300

    filling out the documents application

--- plavsk_library/documents/models.py
+++ plavsk_library/documents/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Documents(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Название документа')
+    doc = models.FileField(upload_to='documents/', verbose_name='Документ')
--- plavsk_library/documents/views.py
+++ plavsk_library/documents/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from documents.models import Documents
 
-# Create your views here.
+
+class ViewDocuments(ListView):
+    model = Documents
