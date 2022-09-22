4596e6aab1fc6f5d1da862f5b82c751da9284797
Kapusta-fairy <dknenorm63@gmail.com>
2022-09-22 11:27:34 +0300

    filling out the gallery app

--- plavsk_library/gallery/views.py
+++ plavsk_library/gallery/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from gallery.models import Gallery
 
-# Create your views here.
+
+class ViewGallery(ListView):
+    model = Gallery
