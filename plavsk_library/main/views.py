01fcf37446a7082152707be932229040a1759232
Kapusta-fairy <dknenorm63@gmail.com>
2022-09-26 08:18:47 +0300

    work on templates for main

--- plavsk_library/main/views.py
+++ plavsk_library/main/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import ListView
+from content import MainTitle, MainContent
 from main.models import ImageLink
 
 
 class ImageMain(ListView):
     model = ImageLink
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Title'] = MainTitle
+        context['Content'] = MainContent
+        return context
