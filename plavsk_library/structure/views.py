c701d38371771fce4073f8a748adfbe46eaea4b6
Kapusta-fairy <dknenorm63@gmail.com>
2022-09-29 11:54:37 +0300

    structure update

--- plavsk_library/structure/views.py
+++ plavsk_library/structure/views.py
@@ -8,5 +8,5 @@ class StaffStructure(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['Department'] = Department.objects.all()
+        context['departments'] = Department.objects.all()
         return context
