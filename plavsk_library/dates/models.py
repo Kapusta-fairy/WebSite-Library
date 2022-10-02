c11fae681662be9c7dccbd029ee88cc2bb792e98
Kapusta-fairy <dknenorm63@gmail.com>
2022-10-02 11:58:27 +0300

    fix html

--- plavsk_library/dates/models.py
+++ plavsk_library/dates/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Dates(models.Model):
     date = models.CharField(max_length=60, verbose_name='Дата события')
     content = models.TextField(verbose_name='Информация о событии')
+    year = models.ForeignKey('years', on_delete=models.PROTECT, verbose_name='Год')
 
     def __str__(self):
         return self.date
@@ -12,3 +13,15 @@ class Dates(models.Model):
         verbose_name = 'дата'
         verbose_name_plural = 'даты'
         ordering = ['date']
+
+
+class Years(models.Model):
+    year = models.IntegerField(max_length=4, verbose_name='Год')
+
+    def __str__(self):
+        return self.year
+
+    class Meta:
+        verbose_name = 'год'
+        verbose_name_plural = 'годы'
+        ordering = ['year']
