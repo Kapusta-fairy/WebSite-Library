bc095006fca630183954ec40ddaf419f4b0a7529
Kapusta-fairy <dknenorm63@gmail.com>
2022-10-02 10:17:33 +0300

    fix media

--- plavsk_library/news/models.py
+++ plavsk_library/news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=40, blank=True, verbose_name='Заголовок')
+    title = models.CharField(max_length=40, verbose_name='Заголовок')
     text = models.TextField(blank=True, verbose_name='Текст')
     image = models.ImageField(upload_to='news/image/', blank=True, verbose_name='Изображение')
     video = models.FileField(upload_to='news/video/', blank=True, verbose_name='Видео')
--- plavsk_library/news/views.py
+++ plavsk_library/news/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import ListView
+from content import news_title
 from news.models import News
 
 
 class LatestNews(ListView):
     model = News
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = news_title
+        return context
